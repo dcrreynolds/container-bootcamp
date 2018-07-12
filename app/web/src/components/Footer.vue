b255a7af2df906a92e4093c7567a1b03dee2651a
David Reynolds <David.Reynolds@microsoft.com>
2018-07-12 11:03:54 -0700

    test 2

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        David Global Blackbelt Team
+        David 2 Global Blackbelt Team
       </div>
       <div class="col-lg-6">
       </div>
